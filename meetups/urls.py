612424ddcaaca161485733354510134219ecebc0
Harry <harrisonnjeru7@gmail.com>
2021-12-02 19:31:02 +0300

    initial commit

---
+++ meetups/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+     path('meetups/', views.index, name='all-meetups'),
+     path('meetup/success', views.confirm_registration, name="confirm-registration"),
+     path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-details')
+
+]
